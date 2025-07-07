e76dd41608ff598e56a85fb0576b920e62a085c5
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-07-07 07:34:33 -0500

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Tushar Munge on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
